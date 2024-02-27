7524b8da07d5053a4b648c9537ca76652856a8b0
jerez007 <jerez.solis@gmail.com>
2024-02-26 22:48:28 -0800

    added context

--- App.js
+++ App.js
@@ -16,15 +16,22 @@ const BottomTabs = createBottomTabNavigator();
 function ExpensesOverview() {
   return (
     <BottomTabs.Navigator
-      screenOptions={{
+      screenOptions={({navigation}) => ({
         headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         headerTintColor: "white",
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         tabBarActiveTintColor: GlobalStyles.colors.accent500,
-        headerRight: ({tintColor}) => (
-          <IconButton icon="add" size={24} color={tintColor} onPress={() => {}} />
-        )
-      }}
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon="add"
+            size={24}
+            color={tintColor}
+            onPress={() => {
+              navigation.navigate("ManageExpense");
+            }}
+          />
+        ),
+      })}
     >
       <BottomTabs.Screen
         name="RecentExpenses"
@@ -57,13 +64,19 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+          headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+          headerTintColor: "white",
+        
+        }}>
           <Stack.Screen
             name="ExpensesOverview"
             component={ExpensesOverview}
             options={{ headerShown: false }}
           />
-          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} options={{
+            presentation: "modal", // on iOS this will open it differently 
+          }} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,17 +1,27 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
+import { useNavigation } from "@react-navigation/native";
 
-const ExpenseItem = ({ description, amount, date}) => {
-function expensePressHandler() {
+const ExpenseItem = ({ id, description, amount, date }) => {
+  const navigation = useNavigation();
 
-}
+  function expensePressHandler() {
+    navigation.navigate("ManageExpense", {
+      expenseId: id
+    });
+  }
 
   return (
-    <Pressable onPress={expensePressHandler} style={({pressed}) => pressed && styles.pressed }>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
-          <Text style={[styles.textBase, styles.description]}>{description}</Text>
+          <Text style={[styles.textBase, styles.description]}>
+            {description}
+          </Text>
           <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
@@ -33,13 +43,13 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     backgroundColor: GlobalStyles.colors.primary500,
     flexDirection: "row",
-    justifyContent: "space-between", 
+    justifyContent: "space-between",
     borderRadius: 6,
     elevation: 3,
     shadowColor: GlobalStyles.colors.gray500,
     shadowRadius: 4,
     shadowOffset: { width: 1, height: 1 },
-    shadowOpacity: 0.4
+    shadowOpacity: 0.4,
   },
   textBase: {
     color: GlobalStyles.colors.primary50,
@@ -52,7 +62,7 @@ const styles = StyleSheet.create({
   amountContainer: {
     paddingHorizontal: 12,
     paddingVertical: 4,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 4,
@@ -61,5 +71,5 @@ const styles = StyleSheet.create({
   amount: {
     color: GlobalStyles.colors.primary500,
     fontWeight: "bold",
-  }
+  },
 });
